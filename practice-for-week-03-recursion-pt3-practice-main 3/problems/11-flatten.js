f900b82783092c816303b0a0d63ae63cea73df0e
Cory Baker <corybaker24@gmail.com>
2022-04-21 20:40:12 -0700

    flatten solved

--- practice-for-week-03-recursion-pt3-practice-main 3/problems/11-flatten.js
+++ practice-for-week-03-recursion-pt3-practice-main 3/problems/11-flatten.js
@@ -10,10 +10,22 @@ flatten([1, 2]); // [1, 2]
 flatten([1, [2, [3]]]); // [1, 2, 3]
 ***********************************************************************/
 
-function flatten(arr) {
-  // Your code here
+function flatten(arr, flat = []) {
+  debugger
+  if (arr.length === 0) return flat;
+debugger
+  if(Array.isArray(arr[0])) {
+    flat.push(...flatten(arr[0].flat()))
+    return flatten(arr.slice(1), flat)
+  } else {
+    flat.push(arr[0])
+    return flatten(arr.slice(1), flat)
+  }
+
 }
-  
+
+console.log(flatten([])); // []
+console.log(flatten([1, 2])); // [1, 2]
+console.log(flatten([1, [2, [3]]])); // [1, 2, 3]
 /**************DO NOT MODIFY ANYTHING UNDER THIS LINE*****************/
 module.exports = flatten;
-  
